fde4e43b4a484bf11363ca2a9ee40fa4ee4f0367
unknown <nakib15-3358@diu.edu.bd>
2023-05-02 10:49:19 +0600

    chef's data send properly

--- Index.js
+++ Index.js
@@ -4,6 +4,25 @@ const port = 5000;
 const chefData = require("../italiano_backend/data/chefs.json");
 const recipeData = require("../italiano_backend/data/recipes.json");
 
+app.get("/", (req, res) => {
+  res.send(chefData);
+});
+
+app.get("/:id/recipes", (req, res) => {
+  const id = parseInt(req.params.id);
+  const allRecipes = recipeData;
+  let filteredRecipes = [];
+  allRecipes.recipes.map((recipe) => {
+    if (recipe.chef_id === id) {
+      filteredRecipes.push(recipe);
+    }
+  });
+  if ( filteredRecipes.length > 0 ) {
+    res.send(filteredRecipes);
+  } else {
+    res.send({"data": "no results"});
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
